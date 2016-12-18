9d5fe7a7d2c083f0f1f1560276999cd7b2a3180a
jstolsvi <jan.stolsvik@soprasteria.com>
2016-12-18 11:07:25 +0100

    fix

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {expect} from "chai";
+import {shallow} from "enzyme";
+import sinon from "sinon";
+
+const TodoList = ({todos, onTodoClick}) => (
+    <ul>
+        {todos.map(todo =>
+            <Todo
+                key={todo.id}
+                {...todo}
+                onClick={() => onTodoClick(todo.id)}
+            />
+        )}
+    </ul>
+);
+
+const Todo = ({onClick, completed, text}) => (
+    <li
+        onClick={onClick}
+        style={{
+            textDecoration: completed ? 'line-through' : 'none'
+        }}
+    >
+        {text}
+    </li>
+)
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
@@ -9,8 +34,8 @@ it('renders without crashing', () => {
 });
 
 // action types
-export const ADD_TODO = 'ADD_TODO'
-export const TOGGLE_TODO = 'TOGGLE_TODO'
+export const ADD_TODO = 'ADD_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
 
 // actions
 export function addTodo(text) {
@@ -31,7 +56,7 @@ function todos(state = [], action) {
                     text: action.text,
                     completed: false
                 }
-            ]
+            ];
         case TOGGLE_TODO:
             return state.map((todo, index) => {
                 if (index === action.index) {
@@ -40,7 +65,7 @@ function todos(state = [], action) {
                     })
                 }
                 return todo
-            })
+            });
         default:
             return state
     }
@@ -48,8 +73,7 @@ function todos(state = [], action) {
 
 
 it('can add todo', () => {
-    const result = todos([], addTodo("new todo"))
-    console.log(result);
+    const result = todos([], addTodo("new todo"));
     expect(result).to.deep.equal([
         {
             text: "new todo",
@@ -59,12 +83,34 @@ it('can add todo', () => {
 });
 
 it('can toggle todo', () => {
-    const result = todos([], toggleTodo(0));
-    console.log(result);
-    expect(result).to.deep.equal([
+    const result = todos([
         {
             text: "new todo",
             completed: false
         }
+    ], toggleTodo(0));
+    expect(result).to.deep.equal([
+        {
+            text: "new todo",
+            completed: true
+        }
     ])
+});
+
+it('renders todos', () => {
+    const todos = shallow(<TodoList todos={ [
+        {
+            text: "new todo",
+            completed: false
+        }
+    ]}/>);
+    console.log(todos.debug());
+    expect(todos.containsMatchingElement(<Todo text="new todo" completed={false}/>)).to.equal(true);
+});
+
+it('invokes callback on click', () => {
+    const onButtonClick = sinon.spy();
+    const todos = shallow(<Todo text="test" completed={false} onClick={onButtonClick}/>);
+    todos.find('li').simulate('click');
+    expect(onButtonClick.calledOnce).to.equal(true);
 });
